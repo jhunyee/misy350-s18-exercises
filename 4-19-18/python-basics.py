12b6113f1fc1b882584e8a5233236a6f43f4c60b
Jhunyee Lin <jhunyee@gmail.com>
2018-04-19 10:23:41 -0400

    add exercises

---
+++ 4-19-18/python-basics.py
@@ -0,0 +1,9 @@
+# this is my first python code
+greeting = "hello"
+name = "Jhunyee"
+print greeting #this print out a string
+print len(greeting)
+print greeting.upper()
+print greeting[4]
+print greeting + " " + name
+print "%s %s!" %(greeting,name)
