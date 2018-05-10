02811ce6d2ea19fec11180c32d30dc03b138fd51
Jhunyee Lin <jhunyee@gmail.com>
2018-05-10 10:36:53 -0400

    database

--- songbase/manage.py
+++ songbase/manage.py
@@ -1,12 +1,21 @@
 from flask_script import Manager
-from songbase import app, db
+from songbase import app, db, Artist
 
 manager = Manager(app)
 
 @manager.command
 def deploy():
+    print "resetting database..."
     db.drop_all()
     db.create_all()
+
+    print "inserting initial data..."
+    coldplay = Artist(name="coldplay", about="this is coldplay")
+    maroon5 = Artist(name="maroon 5", about="this is maroon 5")
+    db.session.add(coldplay)
+    db.session.add(maroon5)
+
+    db.session.commit()
 
 if __name__ == '__main__':
     manager.run()
--- songbase/songbase.py
+++ songbase/songbase.py
@@ -27,6 +27,11 @@ def index():
     #return "hello World"
     return render_template('index.html')
 
+@app.route('/artists')
+def all_artists():
+    artists = Artist.query.all()
+    return render_template('all-artists.html', artists=artists)
+
 @app.route('/form', methods=['GET', 'POST'])
 def form():
 
@@ -42,6 +47,16 @@ def form():
 def users(username):
     #return "<h1>hello %s</h1>" % username
     return render_template('user.html', uname=username)
+
+@app.route('/artist/edit/<int:id>')
+def artist_edit(id):
+    artist = Artist.query.filter_by(id=id).first()
+    artist.about = "this is harry wang"
+    db.session.commit()
+
+    return "this is artist %d updated" % id
+    #return render_template('all-artists.html')
+
 
 
 @app.route('/user')
